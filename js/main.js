989103b59a1b12e57a05bb6fefe1fb1b68ae7447
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-03-19 22:37:29 +0600

    this is my first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+	$('#demo').RollingSlider({
+				showArea:"#example",
+				prev:"#jprev",
+				next:"#jnext",
+				moveSpeed:200,
+				autoPlay:false
+			});
+   
+   
+   
+  });
